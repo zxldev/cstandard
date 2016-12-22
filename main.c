884c753a8ec27748848411652566024af7501d3b
zxldev <zxldev@live.com>
2016-12-22 23:31:02 +0800

    math

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "src/lession/01/hello.c"
+#include "src/lession/04math/l_math.c"
 
 int main(int argc, char *argv[]){
     printHello();
     printKeywords();
+    printMath();
     return 0;
 }
---
+++ src/lession/04math/l_math.c
@@ -0,0 +1,21 @@
+//
+// Created by root on 12/22/16.
+//
+
+#include <math.h>
+#include <stdio.h>
+
+void printMath(){
+    printf("acos:%f\n",acos(30));
+    printf("asin:%f\n",asin(30));
+    printf("atan:%f\n",atan(30));
+    printf("atan2:%f\n",atan2(30,30));
+    printf("cos:%f\n",cos(30));
+    printf("cosh:%f\n",cosh(30));
+    printf("sin:%f\n",sin(30));
+    printf("sinh:%f\n",sinh(30));
+    printf("exp:%f\n",exp(30));
+    printf("cosh:%f\n",cosh(30));
+    printf("cosh:%f\n",cosh(30));
+    printf("cosh:%f\n",cosh(30));
+}
